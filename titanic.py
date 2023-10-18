8cbc5d58c7b2606dc61022c982d38710232a6c3e
t-anano <ananor@aggies.ncat.edu>
2023-10-18 19:27:54 -0400

    Imports for Titanic Dataset

---
+++ titanic.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import pandas as pd
+from sklearn import ensemble, preprocessing, tree
+from sklearn.metrics import auc, confusion_matrix, roc_auc_score, roc_curve
+
+
+
+
+
