703ffac2454a29e657c136a216eea9151761e59a
t-anano <ananor@aggies.ncat.edu>
2023-10-20 19:06:22 -0400

    EDA Titanic Dataset

--- Titanic Dataset.py
+++ Titanic Dataset.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
+# In[28]:
+
+
+# Ask a question: Classify whether or not an individual survives the Titanic ship catastrophe based on indivdual and ship 
+# characteristics. Will this indivdual survive the Titanic catastrophe? Essentially we want to take passanger information and 
+# predict if this passanger will survive the Titanic. 
+
+# This is a classification question as we are predicitng a label for survival, either they survived or they died. 
+# In our DataFrames X is Features or sample data and y are our labels or targets
 
 
 import matplotlib.pyplot as plt
@@ -30,15 +38,19 @@ from yellowbrick.model_selection import (
 ) 
 
 
-# In[12]:
+df = pd.read_csv(r"C:\Users\alexn\Documents\MachineLearning\titanic\train.csv")
+# df.head()
+# print(df.shape)
+
+# df.describe().iloc[:,:2]
+
+df.isnull().sum()
 
 
-# Ask a question: Classify whether or not an individual survives the Titanic ship catastrophe based on indivdual and ship 
-# characteristics. Will this indivdual survive the Titanic catastrophe? Essentially we want to take passanger information and 
-# predict if this passanger will survive the Titanic. 
+# In[ ]:
 
-# This is a classification question as we are predicitng a label for survival, either they survived or they died. 
-# In our DataFrames X is Features or sample data and y are our labels or targets 
+
+
 
 
 # In[ ]:
